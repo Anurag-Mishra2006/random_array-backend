46c988af8bda484b56ed97954543aa96b3a87637
anurag-mishra2006 <eranurag2006@gmail.com>
2026-03-09 22:48:02 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+function randomArray() {
+    const mini = 10;
+    const maxi = 100;
+    const size = Math.random() * 10;
+    let a = [];
+    for (let i = 0; i < size; i++) {
+        let randomNumber = Math.round(Math.random() * (maxi - mini) + mini);
+        a.push(randomNumber);
+    }
+    return a;
+}
+app.get("/", (req, res) => {
+    const array = randomArray();
+    return res.json({ data: array });
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+//# sourceMappingURL=index.js.map
